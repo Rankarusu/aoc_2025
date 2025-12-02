6e081b5dd1004477ed082f557693fddebbdfe41a
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-02 17:18:14 +0100

    solve day 2 part 1

---
+++ src/Day02.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rankarusu\Aoc2025;
+
+class Day02 extends Day
+{
+  protected function solvePart1(): void
+  {
+    $pattern = '/^(\d+)(\1)$/';
+
+    $content = $this->readFile('02');
+    $ranges = explode(',', $content);
+    $result = [];
+    foreach ($ranges as $range) {
+      [$start, $end] = array_map(fn($a) => floatval($a), explode('-', $range));
+      for ($i=$start; $i <= $end; $i++) { 
+        if(preg_match($pattern, $i) === 1){
+          $result[] = $i;
+        }
+      }
+
+    }
+    var_dump(array_sum($result));
+  }
+
+  protected function solvePart2(): void {}
+}
--- src/main.php
+++ src/main.php
@@ -2,7 +2,7 @@
 
 require_once  __DIR__ . '/../vendor/autoload.php';
 
-use Rankarusu\Aoc2025\Day01;
+use Rankarusu\Aoc2025\Day02;
 
-$day = new Day01();
+$day = new Day02();
 $day->run();
