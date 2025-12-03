ccdd6f6a71b054676a15426f11962cbeda11cbec
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-03 16:23:19 +0100

    solve day 3 part 2

--- src/Day03.php
+++ src/Day03.php
@@ -40,5 +40,38 @@ class Day03 extends Day
         var_dump(array_sum($result));
     }
 
-    protected function solvePart2(): void {}
+    protected function solvePart2(): void {
+        $content = $this->readFile('03');
+        $lines = explode(PHP_EOL, $content);
+        $joltSize = 12;
+
+        $result = [];
+
+        foreach ($lines as $line) {
+            $jolts = [];
+
+            $highest = 0;
+            $highestIndex = -1;
+            while (count($jolts) !== $joltSize) {
+                // no need to go to the last index if we need more digits anyway
+                for ($i = $highestIndex + 1; $i <= strlen($line) - ($joltSize - count($jolts)); $i++) {
+                    $current = intval($line[$i]);
+                    if ($current > $highest) {
+                        $highest = $current;
+                        $highestIndex = $i;
+                    }
+                    if ($current === 9) {
+                        break;
+                    }
+                }
+
+                $jolts[] = $highest;
+                //reset
+                $highest = 0;
+            }
+            $result[] = floatval(implode("", $jolts));
+        }
+
+        var_dump(array_sum($result));
+    }
 }
