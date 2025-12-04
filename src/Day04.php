1946753490d3777604d0a05432097431f52c5b2d
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-04 18:00:50 +0100

    solve day 4 part 1

---
+++ src/Day04.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Rankarusu\Aoc2025;
+
+class Day04 extends Day
+{
+    protected function solvePart1(): void
+    {
+        $content = $this->readFile('04');
+        $lines = array_map(fn($line) => str_split($line), explode(PHP_EOL, $content));
+
+        $rolls = 0;
+
+        for ($i = 0; $i < count($lines); $i++) {
+            for ($j = 0; $j < count($lines[$i]); $j++) {
+                $current = $lines[$i][$j];
+                if ($current === '@' && $this->checkPosition($i, $j, $lines)) {
+                    $rolls++;
+                }
+            }
+        }
+        var_dump($rolls);
+    }
+
+    private function checkPosition(int $x, int $y, array $grid): bool
+    {
+        $neighbors = [];
+
+        $offsets = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, -1],
+            [0, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+        ];
+
+        $maxX = count($grid) - 1;
+        $maxY = count($grid[0]) - 1;
+
+        foreach ($offsets as [$dx, $dy]) {
+            $nx = $x + $dx;
+            $ny = $y + $dy;
+
+            // Skip out-of-bounds cells
+            if ($nx < 0 || $ny < 0 || $nx > $maxX || $ny > $maxY) {
+                continue;
+            }
+            $value = $grid[$nx][$ny];
+
+            if ($value === '@') {
+                $neighbors[] = [
+                    $nx,
+                    $ny,
+                ];
+            }
+        }
+
+        return count($neighbors) < 4;
+    }
+
+    protected function solvePart2(): void {}
+}
--- src/main.php
+++ src/main.php
@@ -4,5 +4,5 @@ namespace Rankarusu\Aoc2025;
 
 require_once  __DIR__ . '/../vendor/autoload.php';
 
-$day = new Day03();
+$day = new Day04();
 $day->run();
