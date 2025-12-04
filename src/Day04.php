a71d9ea5f71ace39bf16349f54d46dd9be808bad
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-04 18:14:07 +0100

    solve day 4 part 2

--- src/Day04.php
+++ src/Day04.php
@@ -44,7 +44,6 @@ class Day04 extends Day
             $nx = $x + $dx;
             $ny = $y + $dy;
 
-            // Skip out-of-bounds cells
             if ($nx < 0 || $ny < 0 || $nx > $maxX || $ny > $maxY) {
                 continue;
             }
@@ -61,5 +60,31 @@ class Day04 extends Day
         return count($neighbors) < 4;
     }
 
-    protected function solvePart2(): void {}
+    protected function solvePart2(): void
+    {
+        $content = $this->readFile('04');
+        $lines = array_map(fn($line) => str_split($line), explode(PHP_EOL, $content));
+
+        $rolls = 0;
+
+
+        do {
+            $removableRolls = [];
+            for ($i = 0; $i < count($lines); $i++) {
+                for ($j = 0; $j < count($lines[$i]); $j++) {
+                    $current = $lines[$i][$j];
+                    if ($current === '@' && $this->checkPosition($i, $j, $lines)) {
+                        $removableRolls[] = [$i, $j];
+                        $rolls++;
+                    }
+                }
+            }
+            foreach ($removableRolls as $r) {
+                $lines[$r[0]][$r[1]] = '.';
+            }
+        } while (count($removableRolls) > 0);
+
+
+        var_dump($rolls);
+    }
 }
