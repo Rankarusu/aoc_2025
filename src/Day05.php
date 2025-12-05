5de3e1aea3dea866665ce90c0b525c68eba24f4c
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-05 13:16:13 +0100

    solve day 5 part 2

--- src/Day05.php
+++ src/Day05.php
@@ -13,8 +13,8 @@ class Day05 extends Day
         $fresh = [];
 
         foreach ($ids as $id) {
-            foreach ($ranges as [$start, $end]){
-                if ($id >= $start && $id <= $end){
+            foreach ($ranges as [$start, $end]) {
+                if ($id >= $start && $id <= $end) {
                     $fresh[] = $id;
                     break;
                 }
@@ -25,5 +25,29 @@ class Day05 extends Day
     }
 
 
-    protected function solvePart2(): void {}
+    protected function solvePart2(): void
+    {
+        [$ranges] = explode("\n\n", $this->readFile('05'));
+        $ranges = array_map(fn($range) => array_map(fn($i) => floatval($i), explode('-', $range)), explode(PHP_EOL, $ranges));
+
+        // sort by start
+        usort($ranges, fn($a, $b) => $a[0] - $b[0]);
+
+        $res = [$ranges[0]];
+
+        for ($i = 1; $i < count($ranges); $i++) {
+            $last = &$res[count($res) - 1];
+            $cur = $ranges[$i];
+
+            //when start of current range is smaller than the last ones end, we just extend it
+            if ($cur[0] <= $last[1]) {
+                $last[1] = max($last[1], $cur[1]);
+            } else {
+                $res[] = $cur;
+            }
+        }
+
+        // +1 because ranges are inclusive on both ends
+        var_dump(array_sum(array_map(fn($i) => $i[1] - $i[0]+1, $res)));
+    }
 }
