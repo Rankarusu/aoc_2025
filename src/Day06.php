6e4d0804de9d06aa88ea062b64b46f44925a60d5
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-06 13:06:27 +0100

    solve day 6 part 2

--- src/Day06.php
+++ src/Day06.php
@@ -30,5 +30,41 @@ class Day06 extends Day
 
     protected function solvePart2(): void
     {
+        $lines = explode(PHP_EOL, $this->readFile('06'));
+        $charMap = array_map(fn($line) => str_split($line), $lines);
+        $result = [];
+
+        $operator = ' ';
+        $columnNums = [];
+        for ($i = 0; $i < count($charMap[0]); $i++) {
+            $col = array_column($charMap, $i);
+            $num = floatval(trim(join(array_slice($col, 0, count($col) - 1))));
+
+            //empty column
+            if ($num == 0) {
+                continue;
+            }
+
+            $newOp = $col[count($col) - 1];
+            if ($newOp !== ' ' && $i !== 0) {
+
+                if ($operator == '+') {
+                    $result[] = array_sum($columnNums);
+                } else {
+                    $result[] = array_product($columnNums);
+                }
+                $columnNums = [];
+                $operator = $newOp;
+            }
+            $columnNums[] = $num;
+        }
+
+        if ($operator == '+') {
+            $result[] = array_sum($columnNums);
+        } else {
+            $result[] = array_product($columnNums);
+        }
+
+        var_dump(array_sum($result));
     }
 }
