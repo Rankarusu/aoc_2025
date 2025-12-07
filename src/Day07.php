41183cd3ccf7be3ba8d3cfea88fccd96cd81947d
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2025-12-07 12:11:07 +0100

    solve day 7 part 2

--- src/Day07.php
+++ src/Day07.php
@@ -51,5 +51,34 @@ class Day07 extends Day
     }
 
 
-    protected function solvePart2(): void {}
+    /**
+     * helpful visualization:
+     * @link https://www.reddit.com/r/adventofcode/comments/1pgbg8a/2025_day_7_part_2_visualization_for_the_sample/
+     */
+    protected function solvePart2(): void
+    {
+        $lines = explode(PHP_EOL, $this->readFile('07'));
+        $charMap = array_map(fn($line) => str_split($line), $lines);
+
+        $result = array_fill(0, count($charMap[0]), 0);
+
+        $startX = array_find_key($charMap[0], fn($i) => $i === "S");
+        $result[$startX] = 1;
+
+        for ($i = 0; $i < count($charMap); $i++) {
+            for ($j = 0; $j < count($charMap[$i]); $j++) {
+
+                $current = $charMap[$i][$j];
+
+                $currentBeam = $result[$j];
+                if ($current === '^') {
+                    $result[$j + 1] += $currentBeam;
+                    $result[$j - 1] += $currentBeam;
+                    $result[$j] = 0;
+                }
+            }
+        }
+
+        var_dump(array_sum($result));
+    }
 }
